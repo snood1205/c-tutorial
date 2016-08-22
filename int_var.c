948ed4b3f1833dd612bc48da58d2c298f8b1b7d5
Eli Sadoff <snood1205@gmail.com>
2016-08-22 14:10:11 -0400

    int var

---
+++ int_var.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int i;          // integer variable declaration
+
+    i = 123;        // defining integer variable
+
+    printf("value of i is %d", i);
+
+    return 0;
+}
