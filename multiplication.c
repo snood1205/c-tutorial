782b4d5da2a2db81f50474b66ff10d51008231ca
Eli Sadoff <snood1205@gmail.com>
2016-08-22 14:15:57 -0400

    Multiplication

---
+++ multiplication.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int op1, op2, mul;      // variable declaration
+
+    op1 = 5;                // variable definition
+    op2 = 3;
+
+    mul = op1 * op2;        // mulitiplication operation
+
+    printf("Output of %d multiplied by %d is %d", op1, op2, mul);
+}
