55069ccd351b6092eaa3b0d32921359a5d9c6a85
Eli Sadoff <snood1205@gmail.com>
2016-08-22 14:17:19 -0400

    Division

---
+++ division.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int op1, op2, div;      // variable declaration
+
+    op1 = 6;                // variable definition
+    op2 = 3;
+
+    div = 6 / 3;            // division operation
+
+    printf("Output of %d divided by %d is %d", op1, op2, div);
+}
