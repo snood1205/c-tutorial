88ab4d93f2187a9fb0b5a382a2ae27dd7be9f519
Eli Sadoff <snood1205@gmail.com>
2016-08-22 12:58:17 -0400

    bit fields

---
+++ bit_fields.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/* define simple structure */
+struct {
+    unsigned int widhtValidated;
+    unsigned int heightValidated;
+} status1;
+
+/* define a structure with bit fields */
+struct {
+    unsigned int widhtValidated : 1;
+    unsigned int heightValidated : 1;
+} status2;
+
+int main( ) {
+
+    printf( "Memory size occupied by status1 : %d\n", sizeof(status1));
+    printf( "Memory size occupied by status2 : %d\n", sizeof(status2));
+
+    return 0;
+}
