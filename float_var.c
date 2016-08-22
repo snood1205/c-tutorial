89633575768631aedd7e2a691984893b3591ccb3
Eli Sadoff <snood1205@gmail.com>
2016-08-22 14:11:09 -0400

    Float var

---
+++ float_var.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float f;            // floating point variable declaration
+
+    f = 12.001234;      // defining float variable
+
+    printf("value of if is %f", f);
+
+    return 0;
+}
