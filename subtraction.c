271ce3f16527815dee54953ff4d5312aed655fa3
Eli Sadoff <snood1205@gmail.com>
2016-08-22 14:14:43 -0400

    Subtraction

---
+++ subtraction.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int op1, op2, sub;      // variable declaration
+
+    op1 = 5;                // variable definition
+    op2 = 3;
+
+    sub = op1 - op2;        // subtraction operation
+
+    printf("Output of %d - %d is %d", op1, op2, sub);
+}
