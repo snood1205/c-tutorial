64f0b2e6d6a53c49edd4b803296bcb555b4f713b
Eli Sadoff <snood1205@gmail.com>
2016-08-22 14:12:10 -0400

    Double var

---
+++ double_var.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    double d;           // double precision variable declaration
+
+    d = 12.001234;      // defining double precision variable
+
+    printf("value of d is %e\n", d);
+
+    return 0;
+}
