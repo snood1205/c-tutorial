458d96834feb540b863f7bdb76b0a58922ecf5b1
Eli Sadoff <snood1205@gmail.com>
2016-08-22 13:16:21 -0400

    Gets/Puts

---
+++ gets_puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main( ) {
+
+    char str[100];
+
+    printf( "Enter a value :");
+    gets( str );
+
+    printf( "\nYou eneterd: ");
+    puts( str );
+
+    return 0;
+}
